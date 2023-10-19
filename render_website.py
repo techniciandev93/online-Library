1ac67f4b83c51a90239be21801bb471bd41cf43d
anton <a.solovjev@vzv.su>
2023-10-19 13:57:24 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -20,14 +20,39 @@ def on_reload(json_path, template_path, pages_path):
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template(template_path)
-    books = list(chunked(read_json_books(json_path), 10))
+    books = list(chunked(read_json_books(json_path), 4))
+    count_pages = len(books)
     Path(pages_path).mkdir(parents=True, exist_ok=True)
+    paginator = {page: os.path.join(pages_path, f'index{page}.html') for page in range(1, count_pages+1)}
 
     for page, book in enumerate(books, 1):
-        rendered_page = template.render(
-            books=book
-        )
+        previous_url = ''
+        next_url = ''
+        if page <= 1:
+            previous = False
+            next_page = True
+            next_url = os.path.join(pages_path, f'index{page+1}.html')
+        elif page >= count_pages:
+            previous = True
+            previous_url = os.path.join(pages_path, f'index{page-1}.html')
+            next_page = False
+        else:
+            previous = True
+            previous_url = os.path.join(pages_path, f'index{page - 1}.html')
+            next_page = True
+            next_url = os.path.join(pages_path, f'index{page + 1}.html')
+
         file_path = os.path.join(pages_path, f'index{page}.html')
+        rendered_page = template.render(
+            books=book,
+            count_pages=count_pages,
+            current_page=page,
+            paginator=paginator,
+            previous=previous,
+            previous_url=previous_url,
+            next_page=next_page,
+            next_url=next_url
+        )
         with open(file_path, 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
