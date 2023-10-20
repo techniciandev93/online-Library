7126f27da09442c88f4f58a9ecde704d870e1b33
anton <a.solovjev@vzv.su>
2023-10-20 11:29:26 +0300

    Добавлен argparse

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os.path
 from functools import partial
@@ -14,13 +15,13 @@ def read_json_books(path):
     return books
 
 
-def on_reload(json_path, template_path, pages_path, default_html, book_count_page):
+def on_reload(json_path, template_path, pages_path, default_html, books_per_page):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template(template_path)
-    books = list(chunked(read_json_books(json_path), book_count_page))
+    books = list(chunked(read_json_books(json_path), books_per_page))
     count_pages = len(books)
     Path(pages_path).mkdir(parents=True, exist_ok=True)
     paginator = {page: os.path.join(pages_path, f'index{page}.html') for page in range(1, count_pages+1)}
@@ -61,15 +62,29 @@ def on_reload(json_path, template_path, pages_path, default_html, book_count_pag
 
 
 if __name__ == '__main__':
-    json_path = 'books.json'
-    template_path = 'template.html'
-    pages_path = 'pages'
-    default_html = os.path.join(pages_path, 'index.html')
-    book_count_page = 4
+    parser = argparse.ArgumentParser(description="Этот скрипт запускает веб приложение онлайн библиотеки. Запустите "
+                                                 "python render_website.py и перейдите по ссылке "
+                                                 "http://127.0.0.1:5500. Чтобы сформировать json  с книгами запустите "
+                                                 "скрипт python parse_tululu_category.py --start_page 1 --end_page 4 "
+                                                 "Для просмотра параметров используйте python render_website.py --help")
 
-    on_reload_with_args = partial(on_reload, json_path, template_path, pages_path, default_html, book_count_page)
-
+    parser.add_argument('--json_path', type=str, help="Укажите путь к json файлу с книгами", default='books.json')
+    parser.add_argument('--pages_path', type=str, help="Укажите путь к папке где будут лежать html страницы",
+                        default='pages')
+    parser.add_argument('--template_path', type=str, help="Укажите путь к html шаблону",
+                        default='template.html')
+    parser.add_argument('--books_per_page', type=int, help="Укажи какое количество книг выводить на страницу",
+                        default=4)
+    args = parser.parse_args()
+    default_html = os.path.join(args.pages_path, 'index.html')
+    on_reload_with_args = partial(on_reload,
+                                  args.json_path,
+                                  args.json_path,
+                                  args.template_path,
+                                  args.pages_path,
+                                  default_html,
+                                  args.books_per_page)
     on_reload_with_args()
     server = Server()
-    server.watch(template_path, on_reload_with_args)
+    server.watch(args.template_path, on_reload_with_args)
     server.serve(root='.', default_filename=default_html)
